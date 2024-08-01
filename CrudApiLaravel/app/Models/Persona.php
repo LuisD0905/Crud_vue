ff10a685846936adffdef135c1c4dfcb9e9d4b6c
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-08-01 12:36:08 -0600

    Modelo terminado

--- CrudApiLaravel/app/Models/Persona.php
+++ CrudApiLaravel/app/Models/Persona.php
@@ -3,9 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\Model;
 
 class Persona extends Model
 {
     use HasFactory;
+
+    # Nombre de la conexion
+    protected $connection = 'mongodb';
+
+    # Nombre de la tabla / colleccion
+    protected $collection = 'persona';
+
+    # Deshabilitar las fechas de creacion y actualizacion
+    public $timestamps = false;
+
+    # Campos de la tabla
+    protected $fillable = [
+        'nombre',
+        'apellido',
+        'nacionalidad',
+        'edad',
+        'fecha_nacimiento',
+        'nombre_padre',
+        'cedula_padre',
+        'nombre_madre',
+        'cedula_madre'
+    ];
 }
