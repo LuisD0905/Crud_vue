1b0a1bf2853281d65c66dc73a4ff767248060be8
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-08-01 14:31:30 -0600

    Vista de Layout Terminada

--- CrudVue/src/views/layout/LayoutView.vue
+++ CrudVue/src/views/layout/LayoutView.vue
@@ -1,3 +1,31 @@
+<script setup>
+import Menubar from 'primevue/menubar';
+import { RouterView, useRouter } from 'vue-router'
+import { ref } from "vue";
+
+const router = useRouter();
+
+const items = ref([
+    {
+        label: 'Inicio',
+        icon: 'pi pi-home',
+        command: () => {
+            router.push('/');
+        }
+    },
+    {
+        label: 'Personas',
+        icon: 'pi pi-user',
+        command: () => {
+            router.push('/persona');
+        }
+    }
+]);
+</script>
+
 <template>
-    <h1>Hola Mundo</h1>
+    <div style="position: fixed; width: 90%; margin: 10px 5%;">
+        <Menubar :model="items" />
+    </div>
+    <RouterView />
 </template>
