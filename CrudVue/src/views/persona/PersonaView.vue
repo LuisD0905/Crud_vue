1aacc29a96d252570f8a31f093f59d4374c8ab95
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-08-01 14:31:41 -0600

    Vista de Personas Terminada

---
+++ CrudVue/src/views/persona/PersonaView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Card from 'primevue/card';
+</script>
+
+<template>
+    <div style="padding-top: 80px; width: 80%; margin: 0 10%;">
+        <Card>
+            <template #title>Lista de Personas</template>
+            <template #content>
+                <p class="m-0">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error
+                    repudiandae
+                    numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis
+                    esse, cupiditate neque
+                    quas!
+                </p>
+            </template>
+        </Card>
+    </div>
+</template>
