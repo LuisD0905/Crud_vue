b1336953074099ee553554d8b55feae9834de5a1
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-08-01 14:31:22 -0600

    Vista de Home perminada

---
+++ CrudVue/src/views/home/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Card from 'primevue/card';
+</script>
+
+<template>
+    <div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+        <Card>
+            <template #title>
+                <div style="text-align: center;">
+                    Proyecto de Arquitectura de Computadoras 2
+                </div>
+            </template>
+            <template #content>
+                <div style="text-align: center;">
+                    <p style="font-weight: bold;">Presentado por:</p>
+                    <p>Aaron Steve Alfaro Zamora</p>
+                    <p>Daniela Hidalgo Lopez</p>
+                    <p>Luis David Alpizar Castro</p>
+                </div>
+            </template>
+        </Card>
+    </div>
+</template>
