ecf75b584813de16dee62cbc65f4091dc18786e7
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-08-02 00:02:01 -0600

    Interfaces creadas

---
+++ CrudVue/src/interfaces/PersonaInterface.ts
@@ -0,0 +1,13 @@
+export interface Persona
+{
+    _id: string | undefined,
+    nombre: string,
+    apellido: string,
+    nacionalidad: string,
+    edad: number,
+    fecha_nacimiento: string,
+    nombre_padre: string,
+    cedula_padre: string,
+    nombre_madre: string,
+    cedula_madre: string
+}
