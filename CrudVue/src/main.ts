ce9f3a2c5759a56570e46d1acb44bb3a3abb8a78
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-08-02 00:01:38 -0600

    Toast y ConfirmationToast añadido

--- CrudVue/src/main.ts
+++ CrudVue/src/main.ts
@@ -5,10 +5,16 @@ import './main.css'
 
 // Importaciones de PrimeVue
 import PrimeVue from 'primevue/config';
+import ToastService from 'primevue/toastservice';
+import ConfirmationService from 'primevue/confirmationservice';
 import Aura from '@primevue/themes/aura';
 import 'primeicons/primeicons.css'
 
 const app = createApp(App)
+
+// Toast de PrimeVue
+app.use(ToastService);
+app.use(ConfirmationService);
 
 // Tema de Prime
 app.use(PrimeVue, {
